b9738710e417a72b8ec7920125c7961e394b7c82
zjyan <1255015228@qq.com>
2022-02-12 20:18:33 +0800

    提升模块化程度

--- conceptGet/degreeCentrality.py
+++ conceptGet/degreeCentrality.py
@@ -31,25 +31,6 @@ from tools.leaderRank import leaderrank
 from tools.requestKGWork import getPreConcept
 from tools.wordnetTest import getPreAndNextWord
 
-url = '../data/process/水利大辞典-定义-整理数据.json'
-dictRelUrl = '../data/output/水利大辞典-关系-词条2定义.csv'
-levelRelUrl = '../data/output/水利大辞典-关系-下位.csv'
-jsonData = json.load(open(url,encoding='utf-8'))
-requestConcepts = True
-
-# 读取数据阶段
-# 读节点 构造id和name互相映射
-name2id = {}
-id2name = {}
-name2Definition = {}
-namesLexicon = []
-
-for data in jsonData:
-    namesLexicon.append(data['name'])
-    name2id[data['name']] = str(data['id'])
-    id2name[str(data['id'])] = data['name']
-    name2Definition[data['name']] = data['context']
-
 # 读取边数据
 def readRelation(inputPath,relationForArr,relationBackArr,exclusiveIdStart):
     with open(inputPath, 'r', newline='', encoding='utf8') as file_read:  # 打开input_file指定的文件进行只读操作
@@ -205,79 +186,118 @@ def removeNotIn(hySets,namesLexicon):
     for d in delList:
         hySets.pop(d)
     return hySets
-# 定义
-relationForArr = {}  # 出度节点字典
-relationBackArr = {}   #入度节点字典
 
-# 排除一些概念
-exclusiveNameStart = ['地质','工程力学','水力学','河流动力学','土力学','岩石力学','给水排水工程','海洋水文学与海岸动力学','港口','航道','河口','海岸','生态水利','水利管理','水利科技']
-exclusiveIdStart = [name2id[name] for name in exclusiveNameStart]
-exclusives = getExclusiveIds(levelRelUrl,exclusiveIdStart)
-# 读入边数据
-readRelation(dictRelUrl,relationForArr,relationBackArr,exclusives)
-readRelation(levelRelUrl,relationForArr,relationBackArr,exclusives)
 
-# 调用pageRank
-# usePageRank1(relationForArr,relationBackArr)
-result = usePageRank2(relationForArr)
-# leaderrank(G)
-# 调用度中心性算法
-# result =  useDegreeCentrality(relationForArr,relationBackArr)
-resultName = [id2name[index[0]] for index in result]
-print(resultName)
-# 获取前k个词条对应的概念集
-hypernymSets = {} #上位词集合
-hyponymsSets = {} #下位词集合
-limit = len(result)
-limit = 500
-terms = []
-resultLimit = [id2name[res[0]] for res in result[:limit]]
-for ald in result[:limit]:
-    # concepts += getPreConcept(id2name[ald[0]])
-    name = id2name[ald[0]]
-    preSyn,nextSyn = getPreAndNextWord(name,resultLimit)
-    # print(id2name[ald[0]]+'的上下位词分别为：')
-    # print(preSyn)
-    # print(nextSyn)
-    if len(preSyn) > 0:
-        hypernymSets[name] = preSyn
-    if len(nextSyn) > 0:
-        hyponymsSets[name] = nextSyn
+url = '../data/process/水利大辞典-定义-整理数据.json'
+dictRelUrl = '../data/output/水利大辞典-关系-词条2定义.csv'
+levelRelUrl = '../data/output/水利大辞典-关系-下位.csv'
+jsonData = json.load(open(url,encoding='utf-8'))
+isRequestConcepts = True
+isExclusive = True
+isUsePageRank = True
+isSave = True
+isLimit = False
 
-    if requestConcepts:
-        print(name)
-        concepts = getPreConcept(name)
-        if concepts == False:
-            print('请求失败，当前请求到:'+ name)
-            requestConcepts = False
-        elif '词语' in concepts:
-            terms.append(name)
+def generateG(relationForArr):
+    pass
 
-# 在获取上下位概念时就已经做了排除，此处废弃
-# hyponymsSets = removeNotIn(hyponymsSets,namesLexicon)
-# hypernymSets = removeNotIn(hypernymSets,namesLexicon)
-print('上位词：'+str(len(hypernymSets)))
-print(hypernymSets)
-print('下位词：'+str(len(hyponymsSets)))
-print(hyponymsSets)
-# 导出路径
-outputSortWordPath = '../output/java/sortWord.csv'
-outputEntytiesPath = '../output/java/entyties'
-outputHypernymPath = '../output/java/hypernym.csv'
-outputHyponymsPath = '../output/java/hyponyms.csv'
-outputDictHyponymsPath = '../output/java/dictHyponyms.csv'
-outputTermsPath = '../output/java/terms.csv'
-# 保存时也要按照id来，这样才好保持唯一性，因此还要额外保存一个对应的词典，或者直接让java去读json文件
-# 1.排序词条
-sortWord = [res[0] for res in result]
-saveNodes(outputSortWordPath,sortWord)
-# 2.上下位关系
-saveRelationshipAsId(outputHypernymPath,hypernymSets)
-saveRelationshipAsId(outputHyponymsPath,hyponymsSets)
-# 3.词典中的下位关系
-saveRelationship(outputDictHyponymsPath,relationForArr)
-# 4.将所有标为词语的词都单独保存
-saveNodes(outputTermsPath,terms)
-# 5.将实体类都分别保存
-entytiesName = ['水利史','水利科技']
+
+if __name__ == '__main__':
+    # 读取数据阶段
+    # 读节点 构造id和name互相映射
+    name2id = {}
+    id2name = {}
+    name2Definition = {}
+    namesLexicon = []
+
+    for data in jsonData:
+        namesLexicon.append(data['name'])
+        name2id[data['name']] = str(data['id'])
+        id2name[str(data['id'])] = data['name']
+        name2Definition[data['name']] = data['context']
+
+    # 定义
+    relationForArr = {}  # 出度节点字典
+    relationBackArr = {}  # 入度节点字典
+    exclusives = []
+    if isExclusive:
+        # 排除一些概念
+        exclusiveNameStart = ['地质', '工程力学', '水力学', '河流动力学', '土力学', '岩石力学', '给水排水工程', '海洋水文学与海岸动力学', '港口', '航道', '河口', '海岸',
+                              '生态水利', '水利管理', '水利科技']
+        exclusiveIdStart = [name2id[name] for name in exclusiveNameStart]
+        exclusives = getExclusiveIds(levelRelUrl, exclusiveIdStart)
+
+    # 读入边数据
+    readRelation(dictRelUrl, relationForArr, relationBackArr, exclusives)
+    readRelation(levelRelUrl, relationForArr, relationBackArr, exclusives)
+
+    if isUsePageRank:
+        # 调用pageRank
+        result = usePageRank2(relationForArr)
+        # leaderrank(G)
+        # 调用度中心性算法
+        # result =  useDegreeCentrality(relationForArr,relationBackArr)
+    else:
+        G = generateG(relationForArr)
+        result = leaderrank(G)
+    resultName = [id2name[index[0]] for index in result]
+    print(resultName)
+    # 获取前k个词条对应的概念集
+    hypernymSets = {}  # 上位词集合
+    hyponymsSets = {}  # 下位词集合
+    if isLimit:
+        limit = 500
+    else:
+        limit = len(result)
+    terms = []
+    resultLimit = [id2name[res[0]] for res in result[:limit]]
+    for ald in result[:limit]:
+        # concepts += getPreConcept(id2name[ald[0]])
+        name = id2name[ald[0]]
+        preSyn, nextSyn = getPreAndNextWord(name, resultLimit)
+        # print(id2name[ald[0]]+'的上下位词分别为：')
+        # print(preSyn)
+        # print(nextSyn)
+        if len(preSyn) > 0:
+            hypernymSets[name] = preSyn
+        if len(nextSyn) > 0:
+            hyponymsSets[name] = nextSyn
+
+        if isRequestConcepts:
+            print(name)
+            concepts = getPreConcept(name)
+            if concepts == False:
+                print('请求失败，当前请求到:' + name)
+                isRequestConcepts = False
+            elif '词语' in concepts:
+                terms.append(name)
+
+    # 在获取上下位概念时就已经做了排除，此处废弃
+    # hyponymsSets = removeNotIn(hyponymsSets,namesLexicon)
+    # hypernymSets = removeNotIn(hypernymSets,namesLexicon)
+    print('上位词：' + str(len(hypernymSets)))
+    print(hypernymSets)
+    print('下位词：' + str(len(hyponymsSets)))
+    print(hyponymsSets)
+    if isSave:
+        # 导出路径
+        outputSortWordPath = '../output/java/sortWord.csv'
+        outputEntytiesPath = '../output/java/entyties'
+        outputHypernymPath = '../output/java/hypernym.csv'
+        outputHyponymsPath = '../output/java/hyponyms.csv'
+        outputDictHyponymsPath = '../output/java/dictHyponyms.csv'
+        outputTermsPath = '../output/java/terms.csv'
+        # 保存时也要按照id来，这样才好保持唯一性，因此还要额外保存一个对应的词典，或者直接让java去读json文件
+        # 1.排序词条
+        sortWord = [res[0] for res in result]
+        saveNodes(outputSortWordPath, sortWord)
+        # 2.上下位关系
+        saveRelationshipAsId(outputHypernymPath, hypernymSets)
+        saveRelationshipAsId(outputHyponymsPath, hyponymsSets)
+        # 3.词典中的下位关系
+        saveRelationship(outputDictHyponymsPath, relationForArr)
+        # 4.将所有标为词语的词都单独保存
+        saveNodes(outputTermsPath, terms)
+        # 5.将实体类都分别保存
+        entytiesName = ['水利史', '水利科技']
 
