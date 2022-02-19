767ad519e9129ff264202eef3efff41808251973
zjyan <1255015228@qq.com>
2022-02-19 15:34:14 +0800

    初步加入依存句法分析

--- tools/simpleProcess.py
+++ tools/simpleProcess.py
@@ -1,5 +1,8 @@
 import csv
 import json
+import re
+
+from ltp import LTP
 
 jsonUrl = '../data/process/水利大辞典-定义-整理数据.json'
 termUrl = '../output/java/terms.csv'
@@ -17,6 +20,7 @@ outputRiver = '../output/java/河流沟渠.csv'
 
 jsonData = json.load(open(jsonUrl,encoding='utf-8'))
 
+ltp = LTP()
 
 def getData(url):
     dataList = []
@@ -32,6 +36,7 @@ def saveNodes(outputPath,nodes):
         for node in nodes:
             f.write(node + '\n')
     f.close()
+
 # 保存某节点前所有list
 def saveStartNodes(outputPath,nodes,nodeName):
     with open(outputPath, 'w', encoding='utf-8', newline='') as f:
@@ -41,6 +46,7 @@ def saveStartNodes(outputPath,nodes,nodeName):
             if node == nodeName:
                 f.close()
                 return
+
 def saveEndNodes(outputPath,nodes,nodeName):
     flag = True
     with open(outputPath, 'w', encoding='utf-8', newline='') as f:
@@ -52,14 +58,12 @@ def saveEndNodes(outputPath,nodes,nodeName):
             f.write(node + '\n')
             # f.write(name2id[node] + '\n')
 
-
 def transferTerms():
     termsName = getData(termUrl)
     print(termsName)
     termsIds = [name2id[name] for name in termsName]
     print(termsIds)
     saveNodes(outputTerms, termsIds)
-
 
 def getNameFromHistory():
     historyIds = getData(hisUrl)
@@ -68,14 +72,12 @@ def getNameFromHistory():
     # saveStartNodes(outputHisName,historyNames,'管子')
     saveEndNodes(outputRiver,historyNames,'京杭运河')
 
-
 def getNameFromSci():
     sciIds = getData(sciUrl)
     sciNames = [id2name[id] for id in sciIds]
     print(sciNames)
     saveStartNodes(outputSciStartName, sciNames, '意大利结构模型研究所')
     saveEndNodes(outputSciEndName,sciNames,'李仪祉')
-
 
 def getSortNames():
     sortIds = getData(sortUrl)
@@ -84,6 +86,55 @@ def getSortNames():
     print(len(sortNames))
     saveNodes(outputSortWords,sortNames)
 
+def redp():
+    for nameItem in name2Definition:
+        context = name2Definition[nameItem]
+        print(nameItem+':'+context)
+        # 分割句子
+        contextList = context.split('。')
+        # 去掉“亦称...”和“即...”这些,利用正则
+        pattern1 = re.compile(r'^亦称\“.*?\”。')
+        pattern2 = re.compile(r'^即\“.*\”')
+        for contextItem in contextList:
+            flag1 = pattern1.match(contextItem)
+            flag2 = pattern2.match(contextItem)
+            if flag1 == None and flag2 == None and len(contextItem) > len(nameItem):
+                seg, hidden = ltp.seg([contextItem])
+                # 统计出现次数
+                # numFind = 0
+                # realWord = []
+                # for wordName in seg[0]:
+                #     if wordName in namesLexicon and wordName != nameItem:
+                #         realWord.append(wordName)
+                #         numFind += 1
+                # 语义角色标注
+                srl = ltp.srl(hidden)
+                # 依存句法分析
+                dep = ltp.dep(hidden)
+                # 关系表述→状语* 动词 + 补语? 宾语?
+                # v1.0 最简单的提取动词和宾语
+                subject = []
+                object = []
+                relations = []
+                for d in dep[0]:
+                    name = seg[0][d[0]-1]
+                    if d[2] == 'SBV' and name in namesLexicon:
+                        subject.append(name)
+                        # numFind -= 1
+                    elif (d[2] == 'VOB' or d[2] == 'POB') and name in namesLexicon:
+                        object.append(name)
+                        # numFind -= 1
+                    elif d[2] == 'HED':
+                        relations.append(name)
+                # print(relations)
+                # print(object)
+                print('------------------------------------')
+                # print(realWord)
+                print(subject + object)
+                print(relations)
+                # print('实际存在比标出的单词数多了：'+str(numFind))
+                print('------------------------------------')
+        # break
 
 if __name__ == '__main__':
     # 读取数据阶段
@@ -98,8 +149,12 @@ if __name__ == '__main__':
         name2id[data['name']] = str(data['id'])
         id2name[str(data['id'])] = data['name']
         name2Definition[data['name']] = data['context']
+        # 数据处理阶段 添加词典
+    # 添加自定义字典
+    ltp.add_words(words=namesLexicon)
 
     # transferTerms()
-    getNameFromHistory()
+    # getNameFromHistory()
     # getNameFromSci()
     # getSortNames()
+    redp()
